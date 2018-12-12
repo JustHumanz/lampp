c8b114770fa4e2c3ebe381e186899695ec290e92
M. Aldin Yulis Setiawan <43176061+JustHumanz@users.noreply.github.com>
2018-12-13 01:59:30 +0700

    fix many bug

--- pack.sh
+++ pack.sh
@@ -21,13 +21,13 @@ echo 'Check Service'
 lo=$(systemctl is-active nginx)
 if [[ $lo == "active" ]]; then
   echo 'disable nginx'
-  service nginx stop && service nginx disable
+  service nginx stop && service nginx disable || systemctl start nginx && systemctl enable nginx
 fi
 
 lo1=$(systemctl is-active apache2)
 if [[ $lo1 == "active" ]]; then
   echo 'disable apache2'
-  service apache2 stop && service apache2 disable
+  service apache2 stop && service apache2 disable || systemctl start apache2 && systemctl enable apache2
 fi
 
 lo2=$(systemctl is-active mysql)
@@ -39,7 +39,7 @@ fi
 lo3=$(systemctl is-active mariadb)
 if [[ $lo3 == "active" ]]; then
   echo 'disable mariadb'
-  service mariadb stop && service mariadb disable
+  service mariadb stop && service mariadb disable 
 fi
 
 #systemctl stop apache2 mysql nginx mariadb
@@ -49,17 +49,18 @@ apt update && sudo apt install nginx mariadb-server mariadb-client php-fpm curl 
 mkdir /opt/html
 echo "Starting and enable mariadb and nginx"
 service nginx start && service nginx enable
-service mariadb start && service mariadb enable  | service mysql start && service mysql enable
+service mariadb start && service mariadb enable  || service mysql start && service mysql enable
 echo -e "\ny\ny\n$pass\n$pass\ny\ny\ny\ny" | /usr/bin/mysql_secure_installation
 mysql -u root -p$pass -e "GRANT USAGE ON *.* TO '$user'@localhost IDENTIFIED BY '$pass';"
 curl https://raw.githubusercontent.com/JustHumanz/lampp/master/try/1.conf  --silent --output /etc/nginx/conf.d/1.conf
 mv /etc/php/7.2/fpm/pool.d/www.conf /etc/php/7.2/fpm/pool.d/www.conf.backup
 curl https://raw.githubusercontent.com/JustHumanz/lampp/master/try/www.conf  --silent --output /etc/php/7.2/fpm/pool.d/www.conf
 curl https://raw.githubusercontent.com/JustHumanz/lampp/master/try/index.html  --silent --output   /opt/html/index.html
+rm /etc/nginx/sites-enabled/default && rm /etc/nginx/sites-available/default
 echo "<?php phpinfo();" > /opt/html/info.php
 
 apt install phpmyadmin -y
 ln -s /usr/share/phpmyadmin /opt/html
 echo 'Restarting php-fpm and nginx'
-systemctl restart php7.2-fpm.service nginx
+systemctl restart php7.2-fpm.service nginx || service php7.2-fpm restart && service nginx restart
 echo 'Try access http://localhost/phpmyadmin and http://localhost/info.php'
